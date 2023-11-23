21d3277a048e1226c2f6f8af685349547d442a86
maureenius <logicallonelinesss@gmail.com>
2023-11-23 16:15:02 +0900

    initial commit

---
+++ ゲームで学ぶ探索アルゴリズム実践入門/ruby/Game/game.rb
@@ -0,0 +1,13 @@
+module Game
+  class Game
+    def initialize
+      @height = 3
+      @width = 4
+      @end_turn = 4
+    end
+
+    def self.test
+      puts "testtest"
+    end
+  end
+end
---
+++ ゲームで学ぶ探索アルゴリズム実践入門/ruby/main.rb
@@ -0,0 +1,3 @@
+require_relative 'Game/game'
+
+tmp = Game::Game.test
